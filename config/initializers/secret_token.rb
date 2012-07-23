f88d897449ef25c58e0d992942751d9e0df62028
Elaine Marino <elaine.c.marino@gmail.com>
2012-07-23 17:38:06 -0600

    New rails application with Mike!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeastieBoysApp::Application.config.secret_token = '0c96d3a0e91a5cc87101d98314484e246bec29c4e3bf299c4de1a50c833443eb4d4dfc85c7340c9d24ca2c797c7afea26eb81d48077982abd81e550b52c4488d'
