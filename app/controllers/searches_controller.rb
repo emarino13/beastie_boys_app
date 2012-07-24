fb49ae0026f94bf8cb0846dce0ad711d9353f24f
Elaine Marino <elaine.c.marino@gmail.com>
2012-07-23 18:53:49 -0600

    Added search functionality for album name.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,6 +3,7 @@ class SearchesController < ApplicationController
   def index
     @album_year  = params[:album_year]
     @albums = Album.where(:year => @album_year)
+    @album_name = params[:album_name]
 
   end
 
