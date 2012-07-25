a2884aa9e17301f92a61dc1c9afe1a6414aab2b8
Elaine Marino <elaine.c.marino@gmail.com>
2012-07-25 15:47:55 -0600

    Attemped to add a seeds file to replace my database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Album.create! do |album|
+  @albums = {
+    '1986' => 'Licensed to Ill',
+    '1989' => 'Paul\'s Boutique',
+    '1992' => 'Check Your Head',
+    '1994' => 'Ill Communication',
+    '1998' => 'Hello Nasty',
+    '2004' => 'To the 5 Boroughs',
+    '2007' => 'The Mix-up',
+    '2011' => 'Hot Sauce Committee Part Two'
+  }
+
+end
+
+#Album.create!(:name => 'Liscensed to Ill', :year => 1986)
+#Album.create!(album_name: 'Paul\'s Boutique', album_year: 1989)
+#Album.create!(album_name: 'Check Your Head', album_year: 1992)
+#Album.create!(album_name: 'Ill Communication', album_year: 1994)
+#Album.create!(album_name: 'Hello Nasty', album_year: 1998)
+#Album.create!(album_name: 'To the 5 Boroughs', album_year: 2004)
+#Album.create!(album_name: 'The Mix-up', album_year: 2007)
+#Album.create!(album_name: 'Hot Sauce Committee Part Two', album_year: 2011)
